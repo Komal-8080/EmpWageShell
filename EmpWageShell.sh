5756f80139d42c006a3abdca6b2b186fff146ac5
Komal-8080 <shindeg163@gmail.com>
2020-10-18 00:51:41 +0530

    Employee Check UC2

--- EmpWageShell.sh
+++ EmpWageShell.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -x
 
-greetings="WELCOME"
 user=$(whoami)
-echo " $greetings $user to EmployeeMaster"
+echo " Hello $user Welcome to Employee Wage Computation"
+
+empCheck=$((RANDOM%2))
+if [ $empCheck -eq 1 ];
+	then
+	echo "Employee Present"
+	else
+	echo "Employee Absent"
+fi
