44906b037769716957711627b700377515733c10
Komal-8080 <shindeg163@gmail.com>
2020-10-18 01:06:09 +0530

    Daily Wages Calculated UC2

--- EmpWageShell.sh
+++ EmpWageShell.sh
@@ -3,10 +3,15 @@
 user=$(whoami)
 echo " Hello $user Welcome to Employee Wage Computation"
 
+wagePerHour=20
 empCheck=$((RANDOM%2))
-if [ $empCheck -eq 1 ];
-	then
-	echo "Employee Present"
-	else
-	echo "Employee Absent"
-fi
+	if [ $empCheck -eq 1 ];
+		then
+		echo "Employee Present"
+		working_hours=8
+		else
+		echo "Employee Absent"
+		working_hours=0
+	fi
+daily_emp_wage=$((wagePerHour*working_hours))
+echo "Daily Employee Wage is $daily_emp_wage"
