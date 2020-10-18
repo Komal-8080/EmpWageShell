1ee6ce60c9d3cec684bb021fa4f8bc2b3d1d12b0
Komal-8080 <shindeg163@gmail.com>
2020-10-18 19:13:52 +0530

    condition for calculating wage added UC6

--- EmpWageShell.sh
+++ EmpWageShell.sh
@@ -5,22 +5,27 @@ echo " Hello $user Welcome to Employee Wage Computation"
 
 wagePerMonth=0
 wagePerHour=20
-   for (( i=0; i<20; i++ ))
-   do
-    empCheck=$((RANDOM%3))
-	case $empCheck in
-	2)
-		echo "Employee Present"
-		working_hours=8;;
-	1)
-		echo "Employee Present"
-		working_hours=4;;
-	*)
-		echo "Employee Absent"
-		working_hours=0
-	esac
-	daily_emp_wage=$((wagePerHour*working_hours))
-	wagePerMonth=$((wagePerMonth+daily_emp_wage))
-  	echo "Today's wage is $daily_emp_wage"
-   done
-echo "Employee Wage per month is $wagePerMonth"
+numberOfWorkingDaysInMonth=20
+maxHoursInMonth=100
+totalEmpHours=0
+totalWorkingDays=0
+   	while [[ $totalEmpHours -le $maxHoursInMonth && $totalWorkingDays -lt $numberOfWorkingDaysInMonth ]]
+   	do
+    		((totalWorkingDays++))
+    		empCheck=$((RANDOM%3))
+			case $empCheck in
+			2)
+			echo "Employee Present"
+			working_hours=8;;
+			1)
+			echo "Employee Present"
+			working_hours=4;;
+			*)
+			echo "Employee Absent"
+			working_hours=0
+			esac
+		totalEmpHours=$((totalEmpHours+working_hours))
+		echo "Day: $totalWorkingDays EmpHours: $working_hours"
+   	done
+		wagePerMonth=$((totalEmpHours*wagePerHour))
+		echo "Employee Wage per month is $wagePerMonth"
