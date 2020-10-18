6120787a47bd4c014f4a95c42107a9655dfd1e4a
Komal-8080 <shindeg163@gmail.com>
2020-10-19 00:47:08 +0530

    Day with daily earining and total wages is saved

--- EmpWageShell.sh
+++ EmpWageShell.sh
@@ -3,38 +3,44 @@
 user=$(whoami)
 echo " Hello $user Welcome to Employee Wage Computation"
 
-wagePerMonth=0
 wagePerHour=20
 numberOfWorkingDaysInMonth=20
 maxHoursInMonth=100
+wagePerMonth=0
+dailyEmpWage=0
 totalEmpHours=0
 totalWorkingDays=0
-   function working_hours(){
-      dailyEmpWage=$((working_hours*wagePerHour))
-      totalEmpHours=$((totalEmpHours+working_hours))
-}
-   	while [[ $totalEmpHours -le $maxHoursInMonth && $totalWorkingDays -lt $numberOfWorkingDaysInMonth ]]
-   	do
+    function workHours(){
+
+       dailyEmpWage=$((working_hours*wagePerHour))
+       totalEmpHours=$((totalEmpHours+working_hours))
+
+     }
+   	 while [[ $totalEmpHours -le $maxHoursInMonth && $totalWorkingDays -lt $numberOfWorkingDaysInMonth ]]
+   	 do
     		((totalWorkingDays++))
     		empCheck=$((RANDOM%3))
 
+			echo "Day: $totalWorkingDays"
 			case $empCheck in
 			2)
-			echo "Employee Present"
+			echo "Employee Working Hours Fulltime"
 			working_hours=8;;
 			1)
-			echo "Employee Present"
+			echo "Employee Working Hours Parttime"
 			working_hours=4;;
 			*)
 			echo "Employee Absent"
 			working_hours=0
 			esac
-		
+		workHours
 		echo "Today's earning is $dailyEmpWage"
-		working_hours
-		echo "Day: $totalWorkingDays EmpHours: $working_hours"
+		echo "Employee Work Hours: $working_hours"
    	done
 		wagePerMonth=$((totalEmpHours*wagePerHour))
-		echo "Employee Wage per month is $wagePerMonth"
-
+		echo "                                             "
+		echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+		echo "Total Employee Wage per month is $wagePerMonth"
+		echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+		
 
